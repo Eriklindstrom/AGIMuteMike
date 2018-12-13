1be293c1b93917e425499004ef1ee2e2fa0825be
Lucas Grönborg <lucas94gronborg@gmail.com>
2018-12-13 09:12:12 +0100

    DevBuild v1 - Movement without barriers

---
+++ Assets/Scripts/ClickController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Text;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickController : MonoBehaviour {
+
+  public float timerDuration = 2f;  // How long to look at Menu Item before taking action
+  public GameObject LoadingBar;     // The loading circle bar
+  public GameController gameController;
+
+  private float lookTimer = 0f;     // This value will count down from the duration
+  private bool clickedOnce = false; // Bool to know when a button has been clicked once
+
+  // MonoBehaviour Start
+  void Start() {
+
+  }
+
+  // MonoBehaviour Update
+  void Update() {
+      // Bit shift the index of the layer (8) to get a bit mask
+      int layerMask = 1 << 8;
+
+      // This would cast rays only against colliders in layer 8.
+      // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+      layerMask = ~layerMask;
+
+      RaycastHit hit;
+      // Does the ray intersect any objects excluding the player layer
+      if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+      {
+
+          // If this button has been clicked once it can't be clicked again directly
+          if (clickedOnce) {
+              return;
+          }
+
+          lookTimer += Time.deltaTime;
+          LoadingBar.GetComponent<Image>().fillAmount = lookTimer/timerDuration;
+          // Debug.Log("Gaze progress: " + LoadingBar.GetComponent<Image>().fillAmount*100 + "%");
+
+          if (lookTimer > timerDuration) {
+              clickedOnce = true;
+              LoadingBar.GetComponent<Image>().fillAmount = 0f;
+              lookTimer = 0f;
+              // Debug.Log("BUTTON HAS BEEN SELECTED!");
+
+              // Get scene index from clickable area
+              string areaName = hit.transform.name;
+
+              int sceneIndex;
+              string specialScene;
+              if (areaName.Substring(0,1).Equals("_")) {
+                  specialScene = areaName.Substring(1);
+                  gameController.switchScene(specialScene);
+              }
+              else if (areaName.Substring(1,1).Equals(".")) {
+                  sceneIndex = Int32.Parse(areaName.Substring(0,1));
+                  hit.transform.parent.gameObject.SetActive(false);
+                  gameController.switchScene(sceneIndex);
+              }
+              else {
+                  sceneIndex = Int32.Parse(areaName.Substring(0,2));
+                  hit.transform.parent.gameObject.SetActive(false);
+                  gameController.switchScene(sceneIndex);
+              }
+          }
+      }
+      else
+      {
+          clickedOnce = false;
+          LoadingBar.GetComponent<Image>().fillAmount = 0f;
+          lookTimer = 0f;
+          // Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+          // Debug.Log("Did not Hit");
+      }
+  }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public VideoController videoController;
+	public GameObject letterClickArea;
+	public GameObject[] sceneList;
+
+	private bool hasKey1 = false;
+	private bool hasKey2 = false;
+	private bool erikTalked = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	// Change video and scene
+	public void switchScene(int sceneIndex) {
+			if(sceneIndex==4 && !hasKey1) {
+					foundKey(1);
+			}
+			else if(sceneIndex==5 && !hasKey2) {
+					foundKey(2);
+			}
+
+			sceneList[sceneIndex].SetActive(true);
+			videoController.ChangeVideo(sceneIndex);
+	}
+
+	public void switchScene(string sceneIndex) {
+			// ADD: Special cases eg. cinematics
+	}
+
+	public void foundKey(int keyNum) {
+			if(keyNum==1) {
+					hasKey1 = true;
+					// Change video in the video list to video without key
+					videoController.ReplaceVideo(1, 0);
+			}
+			else {
+					hasKey2 = true;
+					// Change video in the video list to video without key
+					videoController.ReplaceVideo(8, 5);
+			}
+	}
+
+	public void erikTalk() {
+			videoController.ReplaceVideo(4, 2);	// Replace room 2 video
+			videoController.ReplaceVideo(0, 4);	// Replace room 1 video
+			letterClickArea.GetComponent<BoxCollider>().enabled = true;
+	}
+}
